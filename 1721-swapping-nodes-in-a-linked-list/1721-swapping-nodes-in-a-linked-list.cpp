f63efffb1bb587393ef77173e2d1e648b2e77532
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-04-04 16:06:50 +0530

    Time: 760 ms (75.88%), Space: 180.2 MB (15.97%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,13 +11,16 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
+        // two ptr approach
        ListNode *curr = head , *leftptr = head , *rightptr = head;
-       int count = 1;
-        
+       int count = 1; // because the list is 1 indexed
+       
+        // we traverse through the list & make the two ptrs reach their actual pos
         while(curr){
-            if(count < k) leftptr = leftptr->next;
-            if(count > k) rightptr = rightptr->next;
-            
+            if(count < k) leftptr = leftptr->next; // leftptr will be k distance from the start
+            if(count > k) rightptr = rightptr->next; // rightptr will be k distance from curr ptr 
+                                                     // i.e. k distance from the end
+
             curr = curr->next;
             count++;
         }
