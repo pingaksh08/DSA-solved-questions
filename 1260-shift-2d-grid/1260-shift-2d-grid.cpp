4e998a03463653d90773cd253bea3ed13ce54b45
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-04-11 22:55:31 +0530

    Time: 37 ms (60.97%), Space: 13.9 MB (87.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        // math based ques
+        /*
+         Approach -->
+         Store all the elements in a temporary array,then assign the values to
+         the grid matrix from the array from length l-k.
+        */
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> ans(n , vector<int> (m));
+        int l = n*m;
+        k = k%l; // so k don't go out of bound
         
+        if(k == 0)
+            return grid;
+        
+        int itm[l];
+        int index = 0;
+        // inserting elements in a temp array
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
-                int newcol = (j+k)%m; // updated column index
-                int newrow = (i + (j+k)/m)%n; // updated row index
-                ans[newrow][newcol] = grid[i][j];
+                itm[index] = grid[i][j];
+                index++;
             }
         }
         
-        return ans;
+        index = l-k;
+        // assigning shifted values to the grid
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                grid[i][j] = itm[index];
+                index = (index+1)%l; // so index don't go out of bound
+            }
+        }
         
+        return grid;
     }
 };
