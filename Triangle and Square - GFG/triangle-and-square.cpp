366b9bf2b32049d35e7792a04e567c71aae91d98
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-05-03 18:08:10 +0530

    Added solution - LeetHub

---
+++ Triangle and Square - GFG/triangle-and-square.cpp
@@ -0,0 +1,30 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    int countSquare(int B)
+    {
+        int val = B/2 - 1;
+        return (val*(val+1))/2;
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int B,count;
+    	cin>>B;
+    	Solution ob;
+    	count = ob.countSquare(B);
+    	cout<<count<<endl;
+    }
+}  // } Driver Code Ends
