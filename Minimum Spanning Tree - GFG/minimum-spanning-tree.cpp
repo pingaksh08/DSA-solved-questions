8288c617c1c977154cea0a9002307f696d7a8cd7
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-05-10 09:25:09 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -11,9 +11,10 @@ class Solution
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        vector<int> parent(V,-1);
-        vector<int> key(V , INT_MAX);
-        vector<bool> mstvisit(V , false);
+        // PRIMS'S ALGORITHM
+        vector<int> parent(V,-1); // to store the parent of the node
+        vector<int> key(V , INT_MAX); // to store min. weight 
+        vector<bool> mstvisit(V , false); // to mark the visit of the node in mst
         //min heap
         priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
         
@@ -21,17 +22,19 @@ class Solution
         pq.push({0,0});
         
         while(!pq.empty()){
-            int curr_node = pq.top().second;
+            int curr_node = pq.top().second; // get the node
             pq.pop();
-            mstvisit[curr_node] = true;
+            mstvisit[curr_node] = true; // mark it visited
             
             for(auto it : adj[curr_node]){
                 int node = it[0];
                 int weight = it[1];
+                
+                // if the node is not visited in mst and a smaller wt is found then
                 if(mstvisit[node] == false and weight < key[node]){
-                    key[node] = weight;
-                    parent[node] = curr_node;
-                    pq.push({key[node] , node});
+                    key[node] = weight; // update the min wt
+                    parent[node] = curr_node; // update the parent
+                    pq.push({key[node] , node}); // push the node, wt pair in the pq
                 }
             }
         }
