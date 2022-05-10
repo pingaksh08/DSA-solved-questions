a3cbdab273cac7811feefc1da372a9bb74def878
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-05-11 00:01:43 +0530

    Time: 3 ms (96.82%), Space: 11 MB (58.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -3,24 +3,25 @@ public:
     vector<vector<int>> ans;
     vector<int> v;
     
-    void solve(vector<int>& candidates, int target , int start){
+    void solve(vector<int>& candidates, int target , int index){
         // base case
-        if(target == 0){
-            ans.push_back(v);
+        if(index == candidates.size()){
+            if(target == 0){
+                ans.push_back(v);
+            }
             return;
         }
         
-        for(int i=start ; i<candidates.size() ; i++){
-            if(candidates[i] > target) break; // absurd or invalid value for ans
-            
-            v.push_back(candidates[i]); // inserting values in temp vector
-            solve(candidates , target - candidates[i] , i); // recursion
-            v.pop_back(); // backtracking
+        if(candidates[index] <= target){
+            v.push_back(candidates[index]); // inserting values in temp vector
+            solve(candidates , target - candidates[index] , index); // when element is picked
+            v.pop_back(); // backtracking (deleting prev value as the left recursion in eliminated)
         }
+        
+        solve(candidates , target , index+1); // when element is not picked
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin() , candidates.end());
         solve(candidates , target , 0);
         return ans;
     }
