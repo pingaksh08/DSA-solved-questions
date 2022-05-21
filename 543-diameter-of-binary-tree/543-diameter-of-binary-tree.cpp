2da1a865dd3af030a8a0220d4dea2eda846c8d32
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-05-22 00:46:55 +0530

    Time: 31 ms (7.34%), Space: 20.7 MB (6.56%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,7 +11,14 @@
  */
 class Solution {
 public:
-    
+    /*
+    longest path b/w 2 nodes in a tree is called diameter.
+    it may or may not passes through the root.
+    3 cases -->
+    1. either, dm through root
+    2. or, dm through left sub tree
+    3. or, dm through right sub tree
+    */
     int height(TreeNode *root){
         if(!root) return 0;
         return 1 + max(height(root->left) , height(root->right));
@@ -20,7 +27,7 @@ public:
     int diameterOfBinaryTree(TreeNode* root) {
         if(!root) return 0;
         if(!root->left and !root->right) return 0;
-        int d1 = height(root->left) + height(root->right);
+        int d1 = height(root->left) + height(root->right); 
         int d2 = diameterOfBinaryTree(root->left);
         int d3 = diameterOfBinaryTree(root->right);
         return max({d1 , d2 , d3});
