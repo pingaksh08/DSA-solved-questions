28dddbfd3ac82bef03fb660628761a82ceb59887
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-06-22 19:37:52 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (75.42%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,42 @@
  */
 class Solution {
 public:
-    vector<int> v;
-    
-    void solve(TreeNode *root){
-        if(!root) return;
-        
-        solve(root->left);
-        v.push_back(root->val);
-        solve(root->right);
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        solve(root);
-        return v;
+        /*
+        morris traversal --> threaded binary tree concept
+        create a thread from the rightmost node of the left sub tree to 
+        the curr node of that subtree
+        tc --> O(n)
+        sc --> O(1)
+        */
+        vector<int> ans;
+        TreeNode *curr = root;
+        
+        while(curr){
+            // case 1 : no left subtree exists, simply go right
+            if(!curr->left){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode *prev = curr->left;
+                while(prev->right and prev->right != curr){
+                    prev = prev->right;
+                }
+                // case 2 : rightmost child of left subtree points to null , create thread and                       move curr 
+                if(prev->right == NULL){
+                    prev->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    // case 2 : rightmost child of left subtree points to curr, then break the                            thread and move curr to right 
+                    prev->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        
+        return ans;
     }
 };
