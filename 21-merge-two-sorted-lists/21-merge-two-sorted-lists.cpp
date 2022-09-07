a552cb94b7e2ab196f039ee481b800426a9df557
Pingaksh Pareek <84427102+pingaksh08@users.noreply.github.com>
2022-09-07 14:02:06 +0530

    Time: 7 ms (87.94%), Space: 14.9 MB (42.56%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,27 +11,36 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // merging without extra space
         if(!list1) return list2;
         if(!list2) return list1;
         
-        // pointing list1 and list2 to the smallest and largest
-        if(list1->val > list2->val) swap(list1, list2);
+        ListNode *ans = new ListNode(0);
+        ListNode *temp = ans;
         
-        // head of resultant
-        ListNode *ans = list1;
         while(list1 and list2){
-            // curr will keep the track of the smaller element of both
-            ListNode *curr = NULL;
-            while(list1 and list1->val <= list2->val){
-                curr = list1; // keep going till it is smaller
+            if(list1->val <= list2->val){
+                temp->next = list1;
                 list1 = list1->next;
             }
-            // linking the prev smaller to the next larger one in list2
-            curr->next = list2;
-            swap(list1, list2);
+            else{
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
         }
         
-        return ans;
+        while(list1){
+           temp->next = list1;
+           list1 = list1->next; 
+           temp = temp->next; 
+        }
+        
+        while(list2){
+             temp->next = list2;
+             list2 = list2->next;
+             temp = temp->next;
+        }
+        
+        return ans->next;
     }
 };
